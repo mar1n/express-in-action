a166853b8687f99122f24038dc3d3e90d849ca4d
mar1n <30738216+mar1n@users.noreply.github.com>
2021-02-07 20:49:30 +0000

    simple authentication middleware

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var express = require("express");
+var logger = require("morgan");
 var http = require("http");
 
 var app = express();
 
+app.use(logger("short"));
+
 app.use(function(request, response, next) {
-  console.log("In comes a " + request.method + " to " + request.url);
-  next();
+  var minute = (new Date()).getMinutes();
+  if ((minute % 2) === 0) {
+    next();
+  } else {
+    response.statusCode = 403;
+    response.end("Not authorized.");
+  }
 });
 
 app.use(function(request, response) {
