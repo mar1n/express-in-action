e8de10d5bdfe9340ed19551511fa342a94da43bb
mar1n <30738216+mar1n@users.noreply.github.com>
2021-03-21 17:59:14 +0000

    setting up views with Express

--- app.js
+++ app.js
@@ -4,24 +4,13 @@ var http = require("http");
 
 var app = express();
 
-var publicPath = path.resolve(__dirname, "public");
-app.use(express.static(publicPath));
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get("/", function(request, response) {
-    response.end("Welcome to my homepage!");
-});
-
-app.get("/about", function(request, response) {
-    response.end("Welcome to the about page!");
-});
-
-app.get("/weather", function(request, response) {
-    response.end("The current weather is NICE.");
-});
-
-app.use(function(request, response) {
-    response.statusCode = 404;
-    response.end("404!");
-});
+    response.render("index", {
+        message: "Hey everyone! This is my webpage."
+    })
+})
 
 http.createServer(app).listen(3000);
