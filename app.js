daa80bd47fa8ad03e3ba3ac9806010172b524406
mar1n <30738216+mar1n@users.noreply.github.com>
2021-03-21 21:05:02 +0000

    putting all together in guestbook

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
-var express = require("express");
-var path = require("path");
 var http = require("http");
+var path = require("path");
+var express = require("express");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
 
 var app = express();
+
+var entries = [];
+app.locals.entries = entries;
+
+app.use(logger("dev"));
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", function(request, response) {
-    response.render("index", {
-        message: "Hey everyone! This is my webpage."
-    })
-})
+app.use(bodyParser.urlencoded({ extended: false }));
 
-http.createServer(app).listen(3000);
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/new-entry", function (req, res) {
+  res.render("new-entry");
+});
+
+app.post("/new-entry", function (req, res) {
+  if (!req.body.title || !req.body.body) {
+    res.status(400).send("Entries must have a title and a body.");
+    return;
+  }
+  entries.push({
+    title: req.body.title,
+    body: req.body.body,
+    published: new Date(),
+  });
+  res.redirect("/");
+});
+
+app.use(function (req, res) {
+  res.status(404).render("404");
+});
+
+http.createServer(app).listen(3000, function () {
+  console.log("Guestbook app started.");
+});
