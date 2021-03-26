b970be4296c6bbef1453719af8876883f6b7c8d5
mar1n <30738216+mar1n@users.noreply.github.com>
2021-03-26 19:26:50 +0000

    we provide morgan, express.static middleware

--- static-file-fun/app.js
+++ static-file-fun/app.js
@@ -1,13 +1,34 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+var morgan = require("morgan");
 
 var app = express();
 
-app.use(function(req, res, next) {
-    console.log("Request IP: " + req.url);
-    console.log("Request date: " + new Date());
-    next();
+app.use(morgan("short"));
+// app.use(function(req, res, next) {
+//     console.log("Request IP: " + req.url);
+//     console.log("Request date: " + new Date());
+//     next();
+// });
+
+// app.use(function(req, res, next) {
+//     var filePath = path.join(__dirname, "static", req.url);
+//     fs.exists(filePath, function(exists) {
+//         if(exists) {
+//             res.sendFile(filePath);
+//         } else {
+//             next()
+//         }
+//     })
+// });
+
+var staticPath = path.join(__dirname, "static");
+app.use(express.static(staticPath));
+
+app.use(function(req, res) {
+    res.status(404);
+    res.send("File not found!");
 });
 
 app.listen(3000, function() {
