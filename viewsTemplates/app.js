1cb9d006c317984b9ce18a426dc608e05286f1cf
mar1n <30738216+mar1n@users.noreply.github.com>
2021-04-03 21:22:56 +0100

    all view engines compatible with express

--- viewsTemplates/app.js
+++ viewsTemplates/app.js
@@ -1,33 +1,14 @@
 var express = require("express");
+var engines = require("consolidate");
 var path = require("path");
-var ejs = require("ejs");
-
 var app = express();
 
-app.locals.appName = "Song Lyrics";
-
-app.set("view engin", "jade");
+app.set("view engine", "wal");
+app.engine("wal", engines.walrus);
 app.set("views", path.resolve(__dirname, "views"));
-app.engine("html", ejs.renderFile);
 
-app.use(function(req, res, next) {
-    res.locals.userAgent = req.headers["user-agent"];
-    next();
-});
-
-app.get("/about", function(req, res) {
-    res.render("about");
-});
-
-app.get("/contact", function(req, res) {
-    res.render("contact.ejs");
-});
-
-app.use(function(req, res) {
-    res.status(404);
-    res.render("404.html", {
-        urlAttempted: req.url
-    });
+app.get("/", function(req, res) {
+    res.render("index");
 });
 
 app.listen(3000);
