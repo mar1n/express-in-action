15f08df036421f2165fcfbde9f84f6a8f6f6f4f2
mar1n <30738216+mar1n@users.noreply.github.com>
2021-04-03 18:46:52 +0100

    version2 of api

--- randomNumberProject/app.js
+++ randomNumberProject/app.js
@@ -1,19 +1,25 @@
 var express = require("express");
 
+var apiVersion1 = require("./api1.js");
+var apiVersion2 = require("./api2.js");
+
 var app = express();
 
-app.get("/random/:min/:max", function(req, res) {
-    var min = parseInt(req.params.min);
-    var max = parseInt(req.params.max);
-    if(isNaN(min) || isNaN(max)) {
-        res.status(400);
-        res.json({ error: "Bad request."});
-        return;
-    }
-    var result = Math.round((Math.random() * (max - min)) + min);
+// app.get("/random/:min/:max", function(req, res) {
+//     var min = parseInt(req.params.min);
+//     var max = parseInt(req.params.max);
+//     if(isNaN(min) || isNaN(max)) {
+//         res.status(400);
+//         res.json({ error: "Bad request."});
+//         return;
+//     }
+//     var result = Math.round((Math.random() * (max - min)) + min);
 
-    res.json({ result: result});
-});
+//     res.json({ result: result});
+// });
+
+app.use("/v1", apiVersion1);
+app.use("/v2", apiVersion2);
 
 app.listen(3000, function() {
     console.log("App started on port 3000");
