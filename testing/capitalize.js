7724f1d9fc578a0d30b9113048acb5e8892368cf
mar1n <30738216+mar1n@users.noreply.github.com>
2021-04-11 18:00:08 +0100

    test pass for empty string

--- testing/capitalize.js
+++ testing/capitalize.js
@@ -1,5 +1,5 @@
 function capitalize(str) {
-  var firstLetter = str[0].toUpperCase();
+  var firstLetter = str.charAt(0).toUpperCase();
   var rest = str.slice(1).toLowerCase();
   return firstLetter + rest;
 }
